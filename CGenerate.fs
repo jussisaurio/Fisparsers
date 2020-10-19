ac642bf8b9e2357dc131fc99df057985babdad96
saurio <jussi.saurio.ext@nokia.com>
2020-10-19 08:38:25 +0300

    Fix equal precedence operations to be left-associative

--- CGenerate.fs
+++ CGenerate.fs
@@ -2,6 +2,8 @@ module CGenerate
 
 open CParser
 
+let branch f1 f2 pred x = let op = snd x in if fst x |> pred then f1 op else f2 op
+
 let rec generateCProgram (Program main) =
     match main with
         | func -> generateFunction func
@@ -18,37 +20,29 @@ and generateReturnStatement expr = generateExpression expr + "\nret"
 and generateUnaryOp op fct =
     let generatedFactor = generateFactor fct
     match op with
-    | NegationOp -> generatedFactor + "\n" + "neg %rax"
-    | BitwiseComplOp -> generatedFactor + "\n" + "not %rax"
+    | NegationOp -> generatedFactor + "\n" + "neg %rax\n"
+    | BitwiseComplOp -> generatedFactor + "\n" + "not %rax\n"
     // Is RAX 0? If yes, set ZF (zero flag) on. Set RAX = 0. Set AL (lowest 8 bits of RAX) to 1 if ZF is on.
-    | LogicalNegationOp -> generatedFactor + "\n" + "cmpq $0, %rax" + "\n" + "movq $0, %rax" + "\n" + "sete %al"
+    | LogicalNegationOp -> generatedFactor + "\n" + "cmpq $0, %rax" + "\n" + "movq $0, %rax" + "\n" + "sete %al\n"
 
 and generateFactor f =
     match f with
     | Parenthesized expr -> generateExpression expr
     | UnaryOp (u, fct) -> generateUnaryOp u fct
-    | Const n -> "movq $" + string n + ", %rax"
+    | Const n -> "movq $" + string n + ", %rax\n"
+
+and add term = "push %rax\n" + generateTerm term + "\npop %rcx\n" + "add %rcx, %rax\n"
+and sub term = "push %rax\n" + generateTerm term + "\npop %rcx\n" + "sub %rax, %rcx\n" + "movq %rcx, %rax\n"
+// Left operand is in %rax. Push it to stack. Generate right operand (%rax). Pop left operand to %rcx. Multiply.
+and mul fct = "push %rax\n" + generateFactor fct + "\npop %rcx\n" + "imul %rcx, %rax\n"
+// Left operand is in %rax. Push it to stack. Generate right operand, and move it to %rcx. Pop left operand to %rax. Sign extend %rax to 16 bytes (%rdx+%rax). Divide 16-byte-value by %rcx.
+and div fct = "push %rax\n" + generateFactor fct + "\nmovq %rax, %rcx\n" + "pop %rax\n" + "cqo\n" + "idivq %rcx\n"
 
 and generateTerm (Term (factor, maybeOtherFactors)) =
-    let generatedFactor = generateFactor factor
-    let rest =
-        match maybeOtherFactors with
-        | [] -> ""
-        // Left operand is in %rax. Push it to stack. Generate right operand (%rax). Pop left operand to %rcx. Multiply.
-        | (MultiOp, factor2)::xs ->  "push %rax\n" + generateTerm (Term (factor2, xs)) + "\npop %rcx\n" + "imul %rcx, %rax\n"
-        // Left operand is in %rax. Push it to stack. Generate right operand, and move it to %rcx. Pop left operand to %rax. Sign extend %rax to 8 bytes (%rdx+%rax). Divide 8-byte-value by %rcx.
-        // %rax stores quotient, %rdx stores remainder. We can ignore the remainder because we are working with ints.
-        | (DivOp, factor2)::xs ->  "push %rax\n" + generateTerm (Term (factor2, xs)) + "\nmovq %rax, %rcx\n" + "pop %rax\n" + "cqo\n" + "idivq %rcx"
-    generatedFactor + "\n" + rest
+    generateFactor factor + ((List.map (branch mul div ((=) MultiOp)) maybeOtherFactors) |> String.concat "\n")
 
 and generateExpression (Expr (term, maybeOtherTerms)) =
-    let generatedTerm = generateTerm term
-    let rest =
-        match maybeOtherTerms with
-        | [] -> ""
-        | (PlusOp, term2)::xs -> "push %rax\n" + generateExpression (Expr (term2, xs)) + "\npop %rcx\n" + "add %rcx, %rax\n"
-        | (MinusOp, term2)::xs -> "push %rax\n" + generateExpression (Expr (term2, xs)) + "\npop %rcx\n" + "sub %rax, %rcx\n" + "movq %rcx, %rax\n"
-    generatedTerm + "\n" + rest
+    generateTerm term + ((List.map (branch add sub ((=) PlusOp)) maybeOtherTerms) |> String.concat "\n")
 
 
 let compileCProgram = parseCProgram >> Result.map generateCProgram
